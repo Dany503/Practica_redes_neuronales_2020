41473beb4dbd91d6e2095d0e5bf9b8169650db41
Daniel Gutiérrez <d.gutierrez.reina@gmail.com>
2020-11-19 12:19:08 +0100

    Update clasificador_rpi_fully_connected.py

--- clasificador_rpi_fully_connected.py
+++ clasificador_rpi_fully_connected.py
@@ -2,6 +2,8 @@
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+import numpy as np
+from PIL import Image
 
 model = Sequential()
 model.add(Dense(10, activation='sigmoid', input_shape=(784,)))
@@ -9,28 +11,11 @@ model.add(Dense(10, activation='softmax'))
 
 model.load_weights('fully_connected.h5')
 
-#%% Cargamos los datos y realizamos las transformaciones
+#%% Predecimos una foto
 
-from keras.datasets import mnist
+img = Image.open('nueve.png').convert('L')
+img_array = np.array(img)
+img_array = 255-img_array
+img_array = img_array.reshape(1, -1)
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_test = x_test.reshape(10000, 784)
-x_test = x_test.astype('float32')
-x_test /= 255
-print(x_test.shape)
-
-#%% Realizamos predicciones
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-plt.imshow(x_test[12].reshape(28,28), cmap=plt.cm.binary)
-dato_prueba = x_test[12].reshape(1, -1)
-print(dato_prueba.shape)
-print(y_test[12])
-
-print(np.argmax(model.predict(dato_prueba)))
-plt.show()
-
-
+print("PREDICCIÓN:", np.argmax(model.predict(img_array)))
